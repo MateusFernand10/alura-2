21018e2acf1661c2c2d97d2015d3a11654e681e1
MateusFernand10 <mateusmikethg@gmail.com>
2024-10-10 08:31:23 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'SERIES',
+    'QUAL A MELHOR SERIE?',
+    'ELITE'
+)
+
+criaCartao(
+    'PROFISSÃO',
+    'QUAL SUA FUTURA PROFISSÃO?',
+    'CIRUGIAO'
+)
+
+criaCartao(
+    'CURIOSIDADE',
+    'MUSICA MAIS ESCUTADA?',
+    'TAYLOR SWIFT'
+)
+
+criaCartao(
+    'FATOS',
+    'MELHOR LUGAR?',
+    'PRAIA'
+)
